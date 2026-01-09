4c346cc0845775f37d23ec43bebb4d9fac8dbe8d
Durdona177 <dsayfiddinova58@gmail.com>
2026-01-09 13:37:36 +0500

    comment

---
+++ lesson-1/homework.py
@@ -0,0 +1,15 @@
+#1. Why do you think companies analyze large volumes of data?
+#Companies analyze large volumes of data to make better decisions, understand customers, find trends, and increase profits.
+
+#2. If analyzing and sorting large data manually in Excel is difficult, how do you think Python can help solve this problem?
+#Python helps by automating data analysis, handling large datasets quickly, and reducing manual work and errors compared to Excel.
+
+#3. Imagine you work at a sales company that receives data about 10,000 customer transactions daily. How would you analyze this data?
+# I would use Python to clean the data, summarize sales, group transactions, and create reports or charts for analysis.
+
+#4. In your opinion, what tasks can Python be useful for in BI processes? (e.g., data cleaning, visualization, etc.)
+#Python is useful in BI for data cleaning, data analysis, visualization, reporting, and automation.
+#5. If you wanted to compare a company's profit year by year, how could this be done using Python?
+
+#6. If you don't know Python, what difficulties might you face when working with large datasets?
+#Without Python, working with large datasets becomes slow, manual, error-prone, and difficult to automate.
